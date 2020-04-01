546783bd67e7084d42d27c0b4bacf0896b4644b1
Isu <s.georgiadis.94@gmail.com>
2020-04-01 13:24:37 +1030

    deployed to heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/shrouded-brook-59231", { useNewUrlParser: true });
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
